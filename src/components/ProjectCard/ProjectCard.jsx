eb07ad48e97c07b9881a9c27923d8c605b32b282
Joel <jbisselele@gmail.com>
2025-05-14 12:07:17 +0200

    Alt modifié

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({ cover, title, onClick }) => {
     return (
         <div className="project-card" onClick={onClick}>
             <img src={cover} alt={`${title}`} title={`${title}`} className="project-cover" />
-            <img src={background} className='project-background'/>
+            <img src={background} alt='Fond de couleur grise' className='project-background'/>
         </div>
     );
 };
