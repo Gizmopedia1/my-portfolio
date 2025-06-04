ed1701bb4ddc4ebd1095278e48b5ab8905ec5b61
Joel <jbisselele@gmail.com>
2025-06-04 12:50:56 +0200

    ajout CV

--- src/components/MySkills/MySkills.jsx
+++ src/components/MySkills/MySkills.jsx
@@ -1,12 +1,19 @@
 import './MySkills.scss'
 import Skills  from '../Skills/Skills'
+import CV from '../../assets/CV-JB.jpg'
+import PDF from '../../assets/CV-JB.pdf'
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
+const logoPDF = <FontAwesomeIcon icon={faFilePdf} />
 
 function MySkills() {
     return (
         <div className='my-skills'>
             <h2 className='sous-titre'>Mes compétences</h2>
             <Skills />
+            <a href={PDF} className='download-btn' download="joel-bisselele-CV">{logoPDF} Télécharger CV</a>
         </div>
     )
 }
